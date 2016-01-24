515f4bb3824d2cdf17fdd214b02996f71aa77516
ssantosv <s.delossantos@gmail.com>
2016-01-24 09:10:36 +0100

    Update Matriz.java

--- src/matrices/Matriz.java
+++ src/matrices/Matriz.java
@@ -65,4 +65,84 @@ public class Matriz {
         ret += "]\n";
         return ret;
     }
+    public static Matriz matrizInversaSSV(Matriz a) throws DimensionesIncompatibles {
+if(! a.getDimension().height.equals(a.getDimension().width)) throw new DimensionesIncompatibles("La matriz debe ser cuadrada");
+double det=1/determinanteSSV(a);
+Matriz nmatriz=matrizAdjuntaSSV(a);
+multiplicarMatrizSSV(det,nmatriz);
+return nmatriz;
+}
+
+
+public static Matriz matrizAdjuntaSSV(Matriz a){
+return matrizTranspuestaSSV(matrizCofactoresSSV(a));
+}
+
+public Matriz matrizCofactoresSSV(Matriz a){
+int i, j, k, l, dimensionA;
+dimensionA=a.getDimension().height;
+Matriz nm = new Matriz(dimensionA, dimensionA, false);
+for(i=0;i<dimensionA;i++) {
+    for(j=0;j<dimensionA;j++) {
+        double[][] det=new double[dimensionA-1][dimensionA-1];
+        double detValor;
+        for(k=0;k<dimensionA;k++) {
+            if(k!=i) {
+                for(l=0;l<dimensionA;l++) {
+                    if(l!=j) {
+                        int indice1=k<i ? k : k-1 ;
+                        int indice2=l<j ? l : l-1 ;
+                        det[indice1][indice2]=a.datos[k][l];
+                    }
+                }
+            }
+        }
+        detValor=determinante(det);
+        nm.datos[i][j]=detValor * (double)Math.pow(-1, i+j+2);
+    }
+}
+return nm;
+}
+
+public Matriz matrizTranspuestaSSV(Matriz a){
+int i, j, dimensionA;
+dimensionA=a.getDimension().height;
+Matriz nuevam = new Matriz(dimensionA, dimensionA, false);
+for(i=0; i<dimensionA; i++){
+    for(j=0; j<dimensionA; j++)
+        nuevam.datos[i][j]=a.datos[j][i];
+}
+return nuevam;
+}
+
+public double determinanteSSV(Matriz a){
+int i, j, k, dimensionA;
+double det;
+dimensionA=a.getDimension().height;
+if(dimensionA==2){
+    det=(a.datos[0][0]*a.datos[1][1])-(a.datos[1][0]*a.datos[0][1]);
+    return det;
+}
+double suma=0;
+for(i=0; i<dimensionA; i++){
+Matriz nm = new Matriz(dimensionA-1, dimensionA-1, false);
+    for(int j=0; j<dimensionA; j++){
+        if(j!=i){
+            for(k=1; k<dimensionA; k++){
+                int indice=-1;
+                if(j<i)
+                    indice=j;
+                else if(j>i)
+                    indice=j-1;
+                nm.datos[indice][k-1]=a.datos[j][k];
+            }
+        }
+    }
+    if(i%2==0)
+        suma+=a.datos[i][0] * determinanteSSV(nm);
+    else
+        suma-=a.datos[i][0] * determinanteSSV(nm);
+}
+return suma;
+}
 }
