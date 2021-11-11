f537928b13f241b290ceaf3c1550b95c627fc16a
Sergio Gálvez Rojas <galvez@uma.es>
2021-11-11 10:38:01 +0100

    New format for Social Computing.

---
+++ src/matrices/Driver.java
@@ -0,0 +1,22 @@
+package matrices;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import matrices.DimensionesIncompatibles;
+import matrices.Matriz;
+
+public class Driver {
+
+    public static void main(String[] args) {
+        Matriz m1 = new Matriz(3, 4, true);
+        System.out.println(m1);
+        Matriz m2 = new Matriz(3, 4, true);
+        System.out.println(m2);
+        try {
+            System.out.println(Matriz.sumarDosMatrices(m1, m2));
+        } catch (DimensionesIncompatibles ex) {
+            Logger.getLogger(Driver.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
